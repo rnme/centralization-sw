2cd9a5090f8a7565d3f04959dd080149c365082c
Pablo Da Costa Porto <pdacostaporto@gmail.com>
2020-05-01 21:41:54 -0300

    Error de tipeo en install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 ansible-galaxy role install --role-file=provision/requirements.yml --force
 ansible-galaxy collection install --requirements-file=provision/requirements.yml --force
-ansible-playbook -i provision/environment/production/hosts provision/install.yml
+ansible-playbook -i provision/environments/production/hosts provision/install.yml
