9b3a20cc5a6254d11dfac5e46a8bda082bd7bbfe
Pablo Da Costa Porto <pdacostaporto@gmail.com>
2020-05-10 21:21:15 -0300

    Configurar cantidad de respiradores y cuáles se excluyen en simulador.

--- ventilators-mock/ventilators-mock.sh
+++ ventilators-mock/ventilators-mock.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
-EXCLUDED=$1
+EXCLUDED=()
+
+show_help() {
+  echo "Simulación de reporte de mediciones de respiradores."
+  echo "Uso: $0 [OPCIONES]"
+  echo "Opciones:"
+  echo -e "\t--count\tCantidad de respiradores. (Obligatorio)"
+  echo -e "\t--exclude\t\tID de un respirador que se quiere excluir. Puede indicarse varias veces."
+  echo -e "\t--help\t\t\tEl script muestra este mensaje y no hace más nada."
+  exit
+}
+
+if [ $# -eq 0 ]; then
+  show_help
+fi
+
+while true; do
+  case "$1" in
+    --count ) VENTILATOR_COUNT=$2; shift 2 ;;
+    --exclude ) EXCLUDED+=($2); shift 2 ;;
+    --help ) show_help ;;
+    * ) break ;;
+  esac
+done
+echo Simulando $VENTILATOR_COUNT respiradores
 while :
 do
-  for VENTILATOR in {1..50}
+  for VENTILATOR in $(seq 1 $VENTILATOR_COUNT)
   do
-    if [ $((VENTILATOR)) -eq $((EXCLUDED)) ]
+    if [[ " ${EXCLUDED[@]} " =~ " $VENTILATOR " ]];
     then
       echo Salteando respirador $VENTILATOR
       continue
